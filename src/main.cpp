b07542b89a1b6419e9fec4e50c8c29d5cc6bcae8
Oszau <oszau@mail.ru>
2020-07-01 23:22:21 +0300

    Update main.cpp
    Added configuration from the command line.

--- src/main.cpp
+++ src/main.cpp
@@ -8,15 +8,27 @@
 
 #include "MFrec.h"
 
-int main()
+void cmd() { printf("CMD: ./crack A 63 6\n"); }
+
+//int main()
+int main(int argc, char *argv[])
 {
- 
+  if(argc!=4) { cmd(); return 1; }
+  uint8_t find_key = *argv[1];
+  uint8_t valid_sector = atoi(argv[2]); if(valid_sector < 0 || valid_sector > 63) { cmd(); return 1; }
+  uint8_t shadow_key = atoi(argv[3]); if(shadow_key < 0 || shadow_key > 63) { cmd(); return 1; }
+
     MFrec com;
-    
-    com.crackKey( AUTHENT_A, 63, 6 );
+
+    //  (command, exploit addr (block with known key), attack addr (key to recover), opt key for exploit bloc)
+    //com.crackKey( AUTHENT_A, 63, 6 );
+
+    if(find_key == 'A' || find_key == 'a') com.crackKey( AUTHENT_A, valid_sector, shadow_key );
+    else if(find_key == 'B' || find_key == 'b') com.crackKey( AUTHENT_B, valid_sector, shadow_key);
+         else { com.stop(); return 1; }
 
     com.stop();
-    
+
     return 0;
 
 }
